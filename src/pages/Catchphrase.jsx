b82cedc7650a8a4c941dd7b4c3c30498555099a3
vbrooke78 <vscotter@hotmail.com>
2023-10-23 20:33:29 +0100

    Moved data into separate file

--- src/pages/Catchphrase.jsx
+++ src/pages/Catchphrase.jsx
@@ -1,14 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import catchphrase1 from '../images/Catchphrase 01.png';
-import catchphrase2 from '../images/Catchphrase 02.png';
-import defeat from '../images/defeat.jpeg';
-import first_aid from '../images/first_aid.jpeg';
-import jackpot from '../images/jackpot.jpeg';
-import milkshake from '../images/milkshake.jpeg';
-import sweetheart from '../images/sweetheart.jpeg';
 import revealNoise from '../audio/Catchphrase_Reveal.mp3';
 import { Square } from '../components/Square';
+import { data } from '../data/data';
 import '../styles/catchphrase.css';
 
 export const Catchphrase = ({
@@ -28,39 +22,6 @@ export const Catchphrase = ({
 
   // Create a ref for the audio element
   const revealAudioRef = useRef(null);
-
-  const images = [
-    catchphrase1,
-    catchphrase2,
-    defeat,
-    first_aid,
-    jackpot,
-    milkshake,
-    sweetheart,
-  ];
-
-  const answers = [
-    "Don't throw your toys out of the pram!",
-    'A chip off the old block!',
-    'Defeat',
-    'First aid',
-    'Jackpot',
-    'Milkshake',
-    'Sweetheart',
-  ];
-
-  // const handleSquareClick = (index) => {
-  //   setSquareVisibility((prevVisibility) => {
-  //     const updatedVisibility = [...prevVisibility];
-  //     updatedVisibility[index] = !updatedVisibility[index];
-  //     return updatedVisibility;
-  //   });
-
-  //   // If the square is clicked and hidden, update the highlighted index if needed
-  //   if (squareVisibility[index]) {
-  //     setHighlightedIndex(null);
-  //   }
-  // };
 
   useEffect(() => {
     const highlightRandomSquare = () => {
@@ -128,11 +89,14 @@ export const Catchphrase = ({
       <section className="catchphrase-container">
         <section className="image-container">
           {showAnswer && (
-            <div className="answer">{answers[imageIndex].toUpperCase()}</div>
+            <div className="answer">{data[imageIndex].answer}</div>
           )}
           <section className="grid-container">
-            {/* Render the image */}
-            <img className="image" alt="crime scene" src={images[imageIndex]} />
+            <img
+              className="image"
+              alt="crime scene"
+              src={data[imageIndex].image}
+            />
 
             {/* Render the 9 squares */}
             {squareVisibility.map((visible, index) => (
@@ -140,7 +104,6 @@ export const Catchphrase = ({
                 key={index}
                 visible={visible}
                 highlighted={highlightedIndex === index} // Pass the highlighted state to Square component
-                // onClick={() => handleSquareClick(index)}
                 isFlashing={isFlashing}
                 randomIndex={randomIndex}
                 index={index}
