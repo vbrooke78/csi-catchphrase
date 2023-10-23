c8f55e9b13eadb8daffccad2b35ea92d3e4ccc9a
vbrooke78 <vscotter@hotmail.com>
2023-10-23 20:27:11 +0100

    Created data file

---
+++ src/data/data.js
@@ -0,0 +1,13 @@
+import catchphrase1 from '../images/Catchphrase 01.png';
+import catchphrase2 from '../images/Catchphrase 02.png';
+import catchphrase3 from '../images/Catchphrase 03.png';
+import catchphrase4 from '../images/Catchphrase 04.png';
+
+const data = [
+  { image: catchphrase1, answer: "DON'T THROW YOUR TOYS OUT OF THE PRAM!" },
+  { image: catchphrase2, answer: 'A CHIP OFF THE OLD BLOCK!' },
+  { image: catchphrase3, answer: 'THE EARLY BIRD CATCHES THE WORM!' },
+  { image: catchphrase4, answer: 'ON CLOUD NINE!' },
+];
+
+export { data };
