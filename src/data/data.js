f68f4071663ab7b60d75ea926e9b62740f1e8982
vbrooke78 <vscotter@hotmail.com>
2023-11-14 20:14:29 +0000

    Added new catchphrase

--- src/data/data.js
+++ src/data/data.js
@@ -2,12 +2,14 @@ import catchphrase1 from '../images/Catchphrase 01.png';
 import catchphrase2 from '../images/Catchphrase 02.png';
 import catchphrase3 from '../images/Catchphrase 03.png';
 import catchphrase4 from '../images/Catchphrase 04.png';
+import catchphrase5 from '../images/Catchphrase 05.png';
 
 const data = [
   { image: catchphrase1, answer: "DON'T THROW YOUR TOYS OUT OF THE PRAM!" },
   { image: catchphrase2, answer: 'A CHIP OFF THE OLD BLOCK!' },
   { image: catchphrase3, answer: 'THE EARLY BIRD CATCHES THE WORM!' },
   { image: catchphrase4, answer: 'ON CLOUD NINE!' },
+  { image: catchphrase5, answer: 'AN APPLE A DAY KEEPS THE DOCTOR AWAY!' },
 ];
 
 export { data };
